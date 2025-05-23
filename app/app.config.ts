5f421b8283b693fe42709dc05ffb69e94befec0e
Adam Barclay <adb@symprex.com>
2025-05-23 15:40:03 +0100

    [Config] Set the title / description etc. of the application

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,12 +1,12 @@
 export default defineAppConfig({
-  title: 'Sink',
-  email: 'sink.cool@miantiao.me',
-  github: 'https://github.com/ccbikai/sink',
+  title: 'Symprex Go',
+  email: 'help@symprex.com',
+  github: 'https://github.com/symprex/link-shortener',
   twitter: 'https://sink.cool/kai',
   telegram: 'https://sink.cool/telegram',
   mastodon: 'https://sink.cool/mastodon',
   blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+  description: 'A company-wide link shortener',
   image: 'https://sink.cool/banner.png',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
