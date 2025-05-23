f31fdcd009cea4beae442e9e049e1385ba9f7561
Adam Barclay <adb@symprex.com>
2025-05-23 15:37:39 +0100

    [Config] Set home to www.symprex.com directly, working around issue setting in CF dashboard

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
     redirectStatusCode: '301',
     linkCacheTtl: 60,
     redirectWithQuery: false,
-    homeURL: '',
+    homeURL: 'https://www.symprex.com',
     cfAccountId: '',
     cfApiToken: '',
     dataset: 'sink',
